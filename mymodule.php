3ea5c950f8ac2c8ba3e02fb3ae25d689134520f9
Marie <mariepolmontiel@gmail.com>
2014-07-29 21:50:11 +0200

    Update mymodule.php

--- mymodule.php
+++ mymodule.php
@@ -11,7 +11,7 @@ class MyModule extends Module
     $this->version = '1.0';
     $this->author = 'Firstname Lastname';
     $this->need_instance = 0;
-    $this->ps_versions_compliancy = array('min' => '1.5', 'max' => '1.5');
+    $this->ps_versions_compliancy = array('min' => '1.5', 'max' => '1.6');
     $this->dependencies = array('blockcart');
  
     parent::__construct();
